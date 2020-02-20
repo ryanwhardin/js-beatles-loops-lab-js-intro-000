75bc2b804c1b66acfda68eff77866b06d36cd03c
Ryan Hardin <ryanwhardin@gmail.com>
2020-02-20 22:13:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // add solution here
+function theBeatlesPlay(){
+  const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+  const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+  for (const musicians;musicians<3;musicians+=1){
+      console.log('${musicians} plays ${instruments}')
+    }
+  }
+}
